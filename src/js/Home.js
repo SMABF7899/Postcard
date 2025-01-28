6ec9269cc9ba834605359f8e9fb0b706bc556615
Seyed Mohammad Ali Bani Fatemi <smabanifatemi@gmail.com>
2025-01-28 10:32:42 +0330

    Edit

--- src/js/Home.js
+++ src/js/Home.js
@@ -4,9 +4,11 @@ function Home() {
     return (
         <div className="Home">
             <header className="Home-header">
-                <a href={"https://smabf.ir"} target={"_blank"} rel="noopener noreferrer">
-                    <img src={`${process.env.PUBLIC_URL}/SMABF.png`} className="Home-logo" alt="logo"/>
-                </a>
+                <div className="rectangle">
+                    <a id={"smabf-logo"} href={"https://smabf.ir"} target={"_blank"} rel="noopener noreferrer">
+                        <img src={`${process.env.PUBLIC_URL}/SMABF.png`} className="Home-logo" alt="logo"/>
+                    </a>
+                </div>
             </header>
         </div>
     );
